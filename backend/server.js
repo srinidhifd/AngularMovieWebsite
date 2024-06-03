f0e5da202b17fba2f77855c9e31af728ad1b0285
Srinidhi Kulkarni <srinidhi.k2001@gmail.com>
2024-06-03 23:16:43 +0530

    added files

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,16 @@ const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
+// Set default port or use 3000
 const PORT = process.env.PORT || 3000;
 
+// Use default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
+
+// Use the router with custom routes
 server.use(router);
 
+// Start the server
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
 });
